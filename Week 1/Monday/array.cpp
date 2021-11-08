c8eaeff174fc128b1159af5fe10c6ad2e98209dc
wupsi <codehook25@gmail.com>
2021-11-08 19:44:17 +0600

    2d array smples

---
+++ Week 1/Monday/array.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n, m; cin >> n >> m;
+    int arr[n][m];
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// i = 0
+// j = 0, j = 1, j = 2, j = 3;
+
+    // cout << endl;
+
+// i = 1
+// j = 0, j = 1, j = 2, j = 3;
+
+    // cout << endl;
+
+// i = 2
+// j = 0, j = 1, j = 2, j = 3;
+
+    // cout << endl;
+
+// i = 3
+// j = 0, j = 1, j = 2, j = 3;
+
+    // cout << endl;
